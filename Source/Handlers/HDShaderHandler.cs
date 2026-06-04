b3e28cab5ebe5dd50fd29af25cb21385425f020b
crckrbrrs <crackedberries@gmail.com>
2026-06-04 18:43:43 +0300

    add entity build mode
    selections don't clear on death and this is super janky but i need this on my other pc

--- Source/Handlers/HDShaderHandler.cs
+++ Source/Handlers/HDShaderHandler.cs
@@ -185,3 +185,122 @@ public static class HDShaderHandler {
 		Draw.SpriteBatch.End();
 	}
 }
+
+// TODO i have like no clue where to put this
+[Submodule]
+public static class SpecialBuffers {
+	internal static void ApplyHooks() {
+		IL.Celeste.Level.Render += IL_LevelRender_RenderToSpecialBuffers;
+		IL.Celeste.LightingRenderer.BeforeRender += IL_LightingRendererBeforeRender_RenderWithoutBlur;
+		On.Celeste.Level.Begin += On_LevelBegin_InitSpecialBuffers;
+		On.Celeste.Level.End += On_LevelEnd_UnloadSpecialBuffers;
+	}
+
+	internal static void RemoveHooks() {
+		IL.Celeste.Level.Render -= IL_LevelRender_RenderToSpecialBuffers;
+		IL.Celeste.LightingRenderer.BeforeRender -= IL_LightingRendererBeforeRender_RenderWithoutBlur;
+		On.Celeste.Level.Begin -= On_LevelBegin_InitSpecialBuffers;
+		On.Celeste.Level.End -= On_LevelEnd_UnloadSpecialBuffers;
+	}
+
+	public static void On_LevelBegin_InitSpecialBuffers(On.Celeste.Level.orig_Begin orig, Level self) {
+		orig(self);
+		Init();
+	}
+
+	public static void On_LevelEnd_UnloadSpecialBuffers(On.Celeste.Level.orig_End orig, Level self) {
+		orig(self);
+		Unload();
+	}
+
+	internal static void IL_LightingRendererBeforeRender_RenderWithoutBlur(ILContext il) {
+		ILCursor cursor = new ILCursor(il);
+
+		cursor.GotoNext(MoveType.Before, cursor => cursor.MatchCallOrCallvirt(typeof(GaussianBlur).GetMethod("Blur")!));
+		cursor.EmitLdsfld(typeof(GameplayBuffers).GetField("Light")!);
+		cursor.EmitDelegate(renderLightWithoutBlur);
+	}
+
+	private static void renderLightWithoutBlur(VirtualRenderTarget source) {
+		Engine.Graphics.GraphicsDevice.SetRenderTarget(Get("light_no_blur"));
+		Engine.Graphics.GraphicsDevice.Clear(Color.Transparent);
+		Draw.SpriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone, null, Matrix.Identity);
+		Draw.SpriteBatch.Draw((Texture2D)source, Vector2.Zero, Color.White);
+		Draw.SpriteBatch.End();
+	}
+
+	internal static void IL_LevelRender_RenderToSpecialBuffers(ILContext il) {
+		ILCursor cursor = new ILCursor(il);
+
+		cursor.GotoNext(MoveType.Before,
+			cursor => cursor.MatchLdnull(), 
+			cursor => cursor.MatchCallvirt<GraphicsDevice>("SetRenderTarget")
+		);
+		cursor.Index -= 2;
+
+		// todo clean this up
+		cursor.MoveAfterLabels();
+		cursor.EmitLdarg0();
+		cursor.EmitDelegate(renderToSpecialBuffers);
+	}
+
+	private static void renderToSpecialBuffers(Level level) {
+		Engine.Graphics.GraphicsDevice.SetRenderTarget(Get("player"));
+		Engine.Graphics.GraphicsDevice.Clear(Color.Transparent);
+		Draw.SpriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone, null, level.Camera.Matrix);
+		if (level.Tracker.CountEntities<Player>() > 0) {
+			foreach (Player player in level.Tracker.GetEntities<Player>().Cast<Player>()) {
+				if (player.Visible) {
+					player.Render();
+				}
+			}
+		} else {
+			foreach (PlayerDeadBody body in level.Entities.FindAll<PlayerDeadBody>()) {
+				if (body.Visible) {
+					body.Render();
+				}
+			}
+		}
+		Draw.SpriteBatch.End();
+
+		// if (Engine.Commands.Open) {
+		// 	level.Entities.DebugRender(level.Camera);
+		// }
+
+		Engine.Graphics.GraphicsDevice.SetRenderTarget(Get("particles"));
+		Engine.Graphics.GraphicsDevice.Clear(Color.Transparent);
+		Draw.SpriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone, null, level.Camera.Matrix);
+		level.ParticlesFG.Render();
+		level.Particles.Render();
+		level.ParticlesBG.Render();
+
+		Draw.SpriteBatch.End();
+	}
+
+	private static readonly Dictionary<string, VirtualRenderTarget?> targets = [];
+
+	public static VirtualRenderTarget? Get(string name) {
+		return targets[name];
+	}
+
+	public static void Create(string name, int width, int height) {
+		targets.Add(name, VirtualContent.CreateRenderTarget($"hd-shader-special-target-{name}", width, height));
+	}	
+
+	public static void Init() {
+		Create("empty", 320, 180);
+		Create("player", 320, 180);
+		Create("particles", 320, 180);
+		Create("light_no_blur", 320, 180);
+		// Create("last_frame", 1920, 1080);
+	}
+
+	public static void Unload() {
+		foreach (string target in targets.Keys) {
+			targets[target]?.Dispose();
+		}
+
+		targets.Clear();
+	}
+}
+

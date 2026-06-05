dad4dc26af4e86c3c7ec6a07f083116710799c82
Wellington <17155519+WellingtonWritesCode@users.noreply.github.com>
2026-06-05 08:50:22 -0300

    Aborted YaoiLog.txt: I woulda logged so much yaoi
    this isn't the commit where I got rid of YaoiLog.txt but that's what came to mind

--- Source/Handlers/YaoiLogger.cs
+++ Source/Handlers/YaoiLogger.cs
@@ -1,33 +1,55 @@
+using System;
 using Monocle;
 using System.Linq;
 using Microsoft.Xna.Framework;
 using System.Collections.Generic;
+using System.Reflection;
+using MonoMod.RuntimeDetour;
 
 namespace Celeste.Mod.YaoiHelper.Handlers;
 
 /**
  *  <summary>
- * Logger that outputs to an ingame text box and a separate text file.
+ * Displays every log prefixed with <c>BL/</c> on a text box.
  * </summary>
  */
 [Submodule]
 public static class YaoiLogger {
+    private static Hook? loggerLogHook;
+
+    public static void ClearLog() {
+        LogDisplay.Content = new List<string>();
+    }
+    
     internal static void ApplyHooks() {
         Everest.Events.LevelLoader.OnLoadingThread += On_LoadingThread_AddLogDisplay;
+        loggerLogHook = new Hook(
+                typeof(Logger).GetMethod("Log", BindingFlags.Static | BindingFlags.Public, 
+                    [typeof(LogLevel), typeof(string), typeof(string)])
+                    ?? throw new MissingMethodException(nameof(Logger), "Log"),
+                On_Logger_Log
+            );
     }
 
     internal static void RemoveHooks() {
         Everest.Events.LevelLoader.OnLoadingThread -= On_LoadingThread_AddLogDisplay;
+        loggerLogHook?.Dispose();
+        loggerLogHook = null;
     }
 
     internal static void On_LoadingThread_AddLogDisplay(Level level) {
         level.Add(new LogDisplay());
     }
+    internal static void On_Logger_Log(Action<LogLevel, string, string> orig, LogLevel level, string tag, string str) {
+        if(tag.StartsWith("BL/")) 
+            LogDisplay.AddLog($"[{tag.Substring(3)}] [{level.ToString()}] {str}");
+        orig(level, tag, str);
+    }
 }
 
 public sealed class LogDisplay : Entity {
     private const int maxLines = 20;
-    private const float maxWidth = 1200;
+    private const float maxLineLength = 130;
     private const float fontSize = .4f;
     private static readonly Vector2 padding = new Vector2(7.5f, 5f);
     public static List<string> Content = new List<string>();
@@ -39,12 +61,34 @@ public sealed class LogDisplay : Entity {
     }
 
     public static void AddLog(string log) {
-        if (ActiveFont.Measure(log).X * fontSize + padding.X * 2 > maxWidth) {
-            //TODO figure out what the fuck to do with this
+        if (log.Length > maxLineLength) {
+            string[] words = log.Split(" ");
+            if (words.Length == 1) {
+                Content.Add("Bro what");
+                return;
+            }
+            List<string> lines = new List<string>();
+            string currentLine = words[0];
+            for (int i = 1; i < words.Length; i++) {
+                if ($"{currentLine} {words[i]}".Length > maxLineLength) {
+                    lines.Add(currentLine);
+                    currentLine = words[i];
+                } else {
+                    currentLine += $" {words[i]}";
+                }
+            }
+            lines.Add(currentLine);
+            foreach (string line in lines) {
+                pushLine(line);
+            }
             return;
         }
+        pushLine(log);
+    }
+
+    private static void pushLine(string line) {
         if(Content.Count == maxLines) Content.RemoveAt(0);
-        Content.Add(log);
+        Content.Add(line);
     }
 
     private void writeLine(string line, Vector2 position) {

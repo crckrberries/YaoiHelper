98dc2c04fb1d5c0ec36d8070aeba7190c908f3cb
Wellington <17155519+WellingtonWritesCode@users.noreply.github.com>
2026-06-06 17:07:34 -0300

    pack it up skittles squad
    here's your gay little settings

--- Source/Handlers/YaoiLogger.cs
+++ Source/Handlers/YaoiLogger.cs
@@ -10,7 +10,7 @@ namespace Celeste.Mod.YaoiHelper.Handlers;
 
 /**
  *  <summary>
- * Displays every log prefixed with <c>BL/</c> on a text box.
+ * Displays every log with a configurable prefix (<c>BL/</c> by default) on a text box.
  * </summary>
  */
 [Submodule]
@@ -18,7 +18,7 @@ public static class YaoiLogger {
     private static Hook? loggerLogHook;
 
     public static void ClearLog() {
-        LogDisplay.Content = new List<string>();
+        LogDisplay.Content = new List<LogLine>();
     }
     
     internal static void ApplyHooks() {
@@ -41,10 +41,15 @@ public static class YaoiLogger {
         level.Add(new LogDisplay());
     }
     internal static void On_Logger_Log(Action<LogLevel, string, string> orig, LogLevel level, string tag, string str) {
-        if(tag.StartsWith("BL/")) 
-            LogDisplay.AddLog($"[{tag.Substring(3)}] [{level.ToString()}] {str}");
+        string prefix = YaoiHelperModule.Settings.LogSubMenu.LogPrefix;
+        if(tag.StartsWith(prefix)) 
+            LogDisplay.AddLog($"[{tag.Substring(prefix.Length)}] [{level.ToString()}] {str}");
         orig(level, tag, str);
     }
+}
+
+public sealed record LogLine(string Line, int TimeLeft) {
+    public int TimeLeft { get; set; } = TimeLeft;
 }
 
 public sealed class LogDisplay : Entity {
@@ -52,7 +57,7 @@ public sealed class LogDisplay : Entity {
     private const float maxLineLength = 130;
     private const float fontSize = .4f;
     private static readonly Vector2 padding = new Vector2(7.5f, 5f);
-    public static List<string> Content = new List<string>();
+    public static List<LogLine> Content = new List<LogLine>();
     private float width, height;
     public LogDisplay() {
         Position.Y = padding.Y;
@@ -64,7 +69,7 @@ public sealed class LogDisplay : Entity {
         if (log.Length > maxLineLength) {
             string[] words = log.Split(" ");
             if (words.Length == 1) {
-                Content.Add("Bro what");
+                pushLine("Bro what");
                 return;
             }
             List<string> lines = new List<string>();
@@ -88,23 +93,34 @@ public sealed class LogDisplay : Entity {
 
     private static void pushLine(string line) {
         if(Content.Count == maxLines) Content.RemoveAt(0);
-        Content.Add(line);
+        Content.Add(new LogLine(line, YaoiHelperModule.Settings.LogSubMenu.LogLifespan*60));
     }
 
     private void writeLine(string line, Vector2 position) {
         ActiveFont.DrawOutline(line, position, Vector2.Zero, Vector2.One*fontSize, Color.White, 2, Color.Black);
     }
-    
+
+    public override void Update() {
+        base.Update();
+        if(YaoiHelperModule.Settings.LogSubMenu.LogLifespan > 0) {
+            foreach (LogLine line in Content) {
+                line.TimeLeft--;
+            }
+
+            Content.RemoveAll(line => line.TimeLeft <= 0);
+        }
+    }
+
     public override void Render() {
         base.Render();
-        if (!YaoiHelperModule.Settings.DisplayLog || Scene is not Level || Content.Count() == 0) return;
-        width = ActiveFont.Measure(Content.Aggregate("", (longest, log) => log.Length > longest.Length ? log : longest)).X * fontSize + padding.X * 2;
+        if (!YaoiHelperModule.Settings.LogSubMenu.DisplayLog || Scene is not Level level || level.FrozenOrPaused || Content.Count() == 0) return;
+        width = ActiveFont.Measure(Content.Aggregate("", (longest, log) => log.Line.Length > longest.Length ? log.Line : longest)).X * fontSize + padding.X * 2;
         height = Content.Count() * 25 + padding.Y * 2;
         Position.X = Engine.Width - width - padding.X;
         Draw.Rect(Position, width, height, new Color(Color.Black,.75f));
         for(int i = 0; i < Content.Count; i++) {
             Vector2 linePosition = Position + new Vector2(padding.X, padding.Y + 25 * i);
-            writeLine(Content[i], linePosition);
+            writeLine(Content[i].Line, linePosition);
         }
     }
 }
--- Source/YaoiHelperModuleSettings.cs
+++ Source/YaoiHelperModuleSettings.cs
@@ -5,13 +5,25 @@ namespace Celeste.Mod.YaoiHelper;
 
 public sealed class YaoiHelperModuleSettings : EverestModuleSettings {
 	public bool BuildAnywhere { get; set; }
-	public bool DisplayLog { get; set; }
-	[YamlIgnore]
-	public bool ClearLog { get; set; }
-	
-	public void CreateClearLogEntry(TextMenu menu, bool inGame) {
-		TextMenu.Button clearLog = new TextMenu.Button(Dialog.Clean("MODOPTIONS_YAOIHELPER_CLEARLOG"));
-		clearLog.Pressed(() => YaoiLogger.ClearLog());
-		menu.Add(clearLog);
+
+	[SettingSubMenu]
+	public class SubMenu0 {
+		public bool DisplayLog { get; set; }
+		[SettingRange(min:0,max:100)]
+		public int LogLifespan { get; set; } = 20;
+		[SettingMinLength(1)]
+		[SettingMaxLength(5)]
+		public string LogPrefix { get; set; } = "BL/";
+		[YamlIgnore]
+		public bool ClearLog { get; set; }
+		
+		public void CreateClearLogEntry(TextMenuExt.SubMenu subMenu, bool inGame) {
+			TextMenu.Button clearLog = new TextMenu.Button(Dialog.Clean("MODOPTIONS_YAOIHELPER_CLEARLOG"));
+			clearLog.Pressed(() => YaoiLogger.ClearLog());
+			subMenu.Add(clearLog);
+		}
 	}
+
+	[SettingName("MODOPTIONS_YAOIHELPER_LOGSUBMENU")]
+	public SubMenu0 LogSubMenu { get; set; } = new();
 }

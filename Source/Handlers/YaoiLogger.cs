77ccd913d2623e7fff91e0b110994533f813b736
Wellington <17155519+WellingtonWritesCode@users.noreply.github.com>
2026-06-07 12:51:28 -0300

    Update YaoiLogger.cs
    make lines fade out when they have 1 second left and reworked line wrapping

--- Source/Handlers/YaoiLogger.cs
+++ Source/Handlers/YaoiLogger.cs
@@ -54,7 +54,7 @@ public sealed record LogLine(string Line, int TimeLeft) {
 
 public sealed class LogDisplay : Entity {
     private const int maxLines = 20;
-    private const float maxLineLength = 130;
+    private const int maxLineLength = 130;
     private const float fontSize = .4f;
     private static readonly Vector2 padding = new Vector2(7.5f, 5f);
     public static List<LogLine> Content = new List<LogLine>();
@@ -68,21 +68,31 @@ public sealed class LogDisplay : Entity {
     public static void AddLog(string log) {
         if (log.Length > maxLineLength) {
             string[] words = log.Split(" ");
-            if (words.Length == 1) {
-                pushLine("Bro what");
-                return;
-            }
             List<string> lines = new List<string>();
-            string currentLine = words[0];
-            for (int i = 1; i < words.Length; i++) {
-                if ($"{currentLine} {words[i]}".Length > maxLineLength) {
+            string currentLine = "";
+            for (int i = 0; i < words.Length; i++) {
+                if (currentLine.Length >= maxLineLength - 1) {
+                    lines.Add(currentLine + (currentLine.Length == maxLineLength ? "" : " "));
+                    currentLine = "";
+                }
+                if (words[i].Length > maxLineLength) {
+                    lines.Add(currentLine+words[i].Substring(0,maxLineLength-currentLine.Length));
+                    words[i] = words[i].Substring(maxLineLength - currentLine.Length);
+                    while (words[i].Length > maxLineLength) {
+                        lines.Add(words[i].Substring(0,maxLineLength));
+                        words[i] = words[i].Substring(maxLineLength);
+                    }
+                    currentLine = "";
+                }
+                if (currentLine.Length == 0) currentLine = words[i];
+                else if ($"{currentLine} {words[i]}".Length > maxLineLength) {
                     lines.Add(currentLine);
                     currentLine = words[i];
                 } else {
                     currentLine += $" {words[i]}";
                 }
             }
-            lines.Add(currentLine);
+            if(currentLine.Length > 0) lines.Add(currentLine);
             foreach (string line in lines) {
                 pushLine(line);
             }
@@ -96,8 +106,15 @@ public sealed class LogDisplay : Entity {
         Content.Add(new LogLine(line, YaoiHelperModule.Settings.LogDisplay.LogLifespan*60));
     }
 
-    private void writeLine(string line, Vector2 position) {
-        ActiveFont.DrawOutline(line, position, Vector2.Zero, Vector2.One*fontSize, Color.White, 2, Color.Black);
+    private void writeLine(string line, Vector2 position, int timeLeft) {
+        float fadeTime = 60;
+        float value;
+        if (YaoiHelperModule.Settings.LogDisplay.LogLifespan > 0 & timeLeft < fadeTime / 5)
+            value = timeLeft / (fadeTime / 5);
+        else
+            value = 1;
+        Color fill = new Color(new Vector3(value));
+        ActiveFont.DrawOutline(line, position, Vector2.Zero, Vector2.One*fontSize, fill, 2, Color.Black);
     }
 
     public override void Update() {
@@ -109,18 +126,21 @@ public sealed class LogDisplay : Entity {
 
             Content.RemoveAll(line => line.TimeLeft <= 0);
         }
+        width = Content.Aggregate(0f, (longest, log) => {
+            float currentWidth = ActiveFont.Measure(log.Line).X;
+            return currentWidth > longest ? currentWidth : longest;
+        }) * fontSize + padding.X * 2;
+        height = Content.Count() * 25 + padding.Y * 2;
+        Position.X = Engine.Width - width - padding.X;
     }
 
     public override void Render() {
         base.Render();
         if (!YaoiHelperModule.Settings.LogDisplay.Enabled || Scene is not Level level || level.FrozenOrPaused || Content.Count() == 0) return;
-        width = ActiveFont.Measure(Content.Aggregate("", (longest, log) => log.Line.Length > longest.Length ? log.Line : longest)).X * fontSize + padding.X * 2;
-        height = Content.Count() * 25 + padding.Y * 2;
-        Position.X = Engine.Width - width - padding.X;
         Draw.Rect(Position, width, height, new Color(Color.Black,.75f));
         for(int i = 0; i < Content.Count; i++) {
             Vector2 linePosition = Position + new Vector2(padding.X, padding.Y + 25 * i);
-            writeLine(Content[i].Line, linePosition);
+            writeLine(Content[i].Line, linePosition, Content[i].TimeLeft);
         }
     }
 }

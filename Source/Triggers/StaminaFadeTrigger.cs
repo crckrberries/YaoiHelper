e4987b6d99e151219a585e80a5e84f2c7537a43b
stella/mae <staegazer@proton.me>
2026-07-28 00:06:22 +0400

    YAoi

--- Source/Triggers/StaminaFadeTrigger.cs
+++ Source/Triggers/StaminaFadeTrigger.cs
@@ -16,57 +16,47 @@ public sealed class StaminaFadeTrigger : Trigger {
     public readonly float LowerBound;
     public readonly float UpperBound;
     
-    public StaminaFadeTrigger(EntityData data, Microsoft.Xna.Framework.Vector2 offset)
-        : base(data, offset)
-    {
+    public StaminaFadeTrigger(EntityData data, Vector2 offset)
+        : base(data, offset) {
         nodes = data.NodesOffset(offset);
         LowerBound = data.Float("lower_bound");
         UpperBound = data.Float("upper_bound");
     }
 
-    public override void Awake(Scene scene)
-    {
+    public override void Awake(Scene scene) {
         base.Awake(scene);
         Trigger = scene.CollideFirst<Trigger>(nodes[0]) ?? scene.Tracker.GetNearestEntity<Trigger>(nodes[0]);
         Trigger?.Collidable = false;
     }
     
-    public override void OnEnter(Player player)
-    {
+    public override void OnEnter(Player player) {
         base.OnEnter(player);
         Trigger?.OnEnter(player);
     }
 
-    public override void OnStay(Player player)
-    {
+    public override void OnStay(Player player) {
         base.OnStay(player);
         Trigger?.OnStay(player);
     }
 
-    public override void OnLeave(Player player)
-    {
+    public override void OnLeave(Player player) {
         base.OnLeave(player);
         Trigger?.OnLeave(player);
     }
 
-    internal static void ApplyHooks()
-    {
+    internal static void ApplyHooks() {
         On.Celeste.Trigger.GetPositionLerp += On_TriggerGetPositionLerp_ApplyStaminaFade;
     }
 
-    internal static void RemoveHooks()
-    {
+    internal static void RemoveHooks() {
         On.Celeste.Trigger.GetPositionLerp -= On_TriggerGetPositionLerp_ApplyStaminaFade;
     }
 
     public static float On_TriggerGetPositionLerp_ApplyStaminaFade(On.Celeste.Trigger.orig_GetPositionLerp orig,
-        Trigger self, Player player, PositionModes mode)
-    {
+        Trigger self, Player player, PositionModes mode) {
         if (self.Scene is not Level level) return orig(self, player, mode);
-        foreach (StaminaFadeTrigger staminaFadeTrigger in level.Tracker.GetEntities<StaminaFadeTrigger>().Cast<StaminaFadeTrigger>())
-        {
-            if (staminaFadeTrigger.Trigger == self)
-            {
+        foreach (StaminaFadeTrigger staminaFadeTrigger in level.Tracker.GetEntities<StaminaFadeTrigger>().Cast<StaminaFadeTrigger>()) {
+            if (staminaFadeTrigger.Trigger == self) {
                 return float.Clamp((player.Stamina - staminaFadeTrigger.LowerBound) / (staminaFadeTrigger.UpperBound - staminaFadeTrigger.LowerBound), 0f, 1f);
             }
         }

7e55b287746e546034d1088354a4b33379a21def
stella/mae <staegazer@proton.me>
2026-07-27 23:35:32 +0400

    I haveth added: Staminae Fade Triggerth to the Esteemede Yaoie Helperth

---
+++ Loenn/triggers/StaminaFadeTrigger.lua
@@ -0,0 +1,12 @@
+return {
+	name = "YaoiHelper/StaminaFadeTrigger",
+	nodeLimits = {1, 1},
+	triggerText = "Stamina Fade",
+	placements = {
+		name = "main",
+		data = {
+			lower_bound = 0,
+			upper_bound = 110
+		}
+	}
+}
